ea0ef2c8aef061af9c36e139373e40fb80b15a54
Gabriel Chagas Fernandes de Moraes <gabrie.moraes4299@gmail.com>
2024-08-30 16:23:15 -0300

    Criação da conexão do Banco de Dados

--- conexao_SQL.py
+++ conexao_SQL.py
@@ -1,1 +1,11 @@
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+import mysql.connector
+class Connection:
+    def conectar():
+        myBD = mysql.connector.connect(
+            host = "127.0.0.1",
+            user="equipe",
+            password="4_b4t@t45_3_m31@",
+            database="bd_easyrequest"
+        )
+
+        return myBD
