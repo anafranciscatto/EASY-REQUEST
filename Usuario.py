fa922c36755f04fd598d5755084beb1c2cfc4354
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-09-05 16:11:10 -0300

    Finalaização do comentário do arquivo Usuario.py

--- Usuario.py
+++ Usuario.py
@@ -1,8 +1,10 @@
+# Importação das funções para conectar o MySQL e criar senhas criptografadas.
 from conexao_SQL import Connection
 from hashlib import sha256
 
+# Criação da classe Usuario, que guarda as informações e funções relacionadas ao usuario.
 class Usuario:
-    def __init__(self) -> None:
+    def __init__(self) -> None: # Função que cria o usuário com suas informações vazias.
         self.CPF = None
         self.nome = None
         self.email = None
@@ -13,7 +15,7 @@ class Usuario:
         self.id_funcao = None
         self.logado = False
 
-    def cadastrar(self, CPF, nome, email, senha, SN, foto, permissao, id_funcao):
+    def cadastrar(self, CPF, nome, email, senha, SN, foto, permissao, id_funcao): # Função que cadastra o usuário no Banco de Dados.
         try:
             myBD = Connection.conectar()
 
@@ -38,7 +40,7 @@ class Usuario:
         except:
             return False
 
-    def logar(self, email, senha, permissao):
+    def logar(self, email, senha, permissao): # Função que executa o login do usuário no sistema.
         myBD = Connection.conectar()
 
         mycursor = myBD.cursor()
