089b4b03ae384b4bf977da01015ae6e1dd189a30
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-09-06 08:57:28 -0300

    Finalização da função que adiciona uma solicitação de serviço no banco

--- Solicitacoes.py
+++ Solicitacoes.py
@@ -1,17 +1,32 @@
 # Importação das funções para conectar o MySQL
 from conexao_SQL import Connection
 
+# Crição da Classe Solicitacoes, que executa as funções relacionadas as solicitações de serviço
 class Solicitacoes:
-    def __init__(self) -> None:
+    def __init__(self) -> None: # Criação de uma solicitação de servico com sua informações vazias.
         self.id_servico = None
         self.id_sala = None
         self.descricao = None
         self.outros_locais = None
         self.CPF_funcionario = None
         
-    def solicitar(self, id_servico, id_sala, descricao, outros_locais, CPF_funcionario):
-        myBD = Connection.conectar()
-        
-        mycursor = myBD.cursor()
-        
-        pass
+    # Função que adiciona uma nova solicitação de servico no Banco de Dados
+    def solicitar_servico(self, id_servico, id_sala, descricao, outros_locais, CPF_funcionario):
+        try:
+            myBD = Connection.conectar()
+            
+            mycursor = myBD.cursor()
+
+            self.id_servico = id_servico
+            self.id_sala = id_sala
+            self.descricao = descricao
+            self.outros_locais = outros_locais
+            self.CPF_funcionario = CPF_funcionario
+            
+            mycursor.execute(f"INSERT INTO tb_solicitacoes (id_servico, id_sala, descricao, outros_locais, CPF_funcionario) VALUES ('{id_servico}','{id_sala}', '{descricao}', '{outros_locais}', '{CPF_funcionario}';")
+
+            myBD.commit()
+
+            return True
+        except:
+            return False
