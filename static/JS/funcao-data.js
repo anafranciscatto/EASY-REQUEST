03336359d49186238c83253d4226ac6a4317e37f
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-09-15 15:05:09 -0300

    Alterando e comentando a função de data

--- static/JS/funcao-data.js
+++ static/JS/funcao-data.js
@@ -1,15 +1,9 @@
+// Função que retorna a data atual atualizada
 const updateDate = () => {
     const now = new Date();
-    const dias = ["Dom.", "Seg.", "Ter.", "Qua.", "Qui.", "Sex.", "Sáb."];
-    const meses = ["Jan.", "Fev.", "Mar.", "Abr.", "Mai.", "Jun.", "Jul.", "Ago.", "Set.", "Out.", "Nov.", "Dez."];
+    const dias = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+    const meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
 
-
-
-    /* Recupare a data atual, formata-a de acordo com a ocalidade pt-BR
-    (dia da semana extenso, ano numérico, mês extenso e dia numérico) */
-
-    // const options = {weekday: 'short', day: 'numeric', month: 'long'};
-    // const formattedDate = now.toLocaleDateString('pt-BR', options);
     document.querySelector('#dia-semana').textContent = dias[now.getDay()];
     document.querySelector('#dia').textContent = now.getDate();
     document.querySelector('#mes').textContent = meses[now.getMonth()];
