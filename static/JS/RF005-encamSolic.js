785021fcac7b5af0863173b565fa51332ddcd4d5
Julia Ribeiro <julia.barros4336@gmail.com>
2024-09-22 19:01:45 -0300

    Fazendo a tela funcionar

--- static/JS/RF005-encamSolic.js
+++ static/JS/RF005-encamSolic.js
@@ -1,18 +1,33 @@
-const options = document.querySelectorAll('.option');
-const urgencyOptions = document.querySelectorAll('.urgency');
-let selectedOptions = [];
-let selectedUrgency = null;
 
-options.forEach(option => {
-  option.addEventListener('click', () => {
-    // Adiciona ou remove a classe 'selected' dependendo do estado atual
-    option.classList.toggle('selected');
+const opcao = document.querySelectorAll('.opcao');
+const opcaoUrgencia  = document.querySelectorAll('.opcaoUrgencia');
 
-    // Adiciona ou remove a opção do array de opções selecionadas
-    if (option.classList.contains('selected')) {
-      selectedOptions.push(option);
+let selecionaOpcao = [];
+let selecionaUrgencia = null;
+
+// seleção dos funcionários
+opcao.forEach(opcao => {
+  opcao.addEventListener('click', () => {
+    // Adiciona ou remove a classe 'selected'
+    opcao.classList.toggle('selected');
+
+    // Adiciona ou remove a opção
+    if (opcao.classList.contains('selected')) {
+     selecionaOpcao.push(opcao);
     } else {
-      selectedOptions = selectedOptions.filter(opt => opt !== option);
+     selecionaOpcao = selecionaOpcao.filter(opt => opt !== opcao);
     }
   });
 });
+
+//seleção da urgencia
+opcaoUrgencia.forEach(opcaoUrgencia =>{
+  opcaoUrgencia.addEventListener('click', () =>{  
+     if (selecionaUrgencia) {
+      selecionaUrgencia.classList.remove('selected');
+    }
+
+    selecionaUrgencia = opcaoUrgencia;
+    opcaoUrgencia.classList.add('selected');
+  })
+})
