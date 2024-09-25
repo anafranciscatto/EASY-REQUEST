efeb66f0f02c81266470f9734223335785c5c7ef
Julia Ribeiro <julia.barros4336@gmail.com>
2024-09-25 18:54:18 -0300

    Coringando

--- static/JS/RF005-encamSolic.js
+++ static/JS/RF005-encamSolic.js
@@ -3,31 +3,41 @@ const opcao = document.querySelectorAll('.opcao');
 const opcaoUrgencia  = document.querySelectorAll('.opcaoUrgencia');
 
 let selecionaOpcao = [];
-let selecionaUrgencia = null;
+let selecionaUrgencia = [];
 
-// seleção dos funcionários
-opcao.forEach(opcao => {
-  opcao.addEventListener('click', () => {
-    // Adiciona ou remove a classe 'selected'
-    opcao.classList.toggle('selected');
 
-    // Adiciona ou remove a opção
-    if (opcao.classList.contains('selected')) {
-     selecionaOpcao.push(opcao);
-    } else {
-     selecionaOpcao = selecionaOpcao.filter(opt => opt !== opcao);
-    }
+function selecaoFuncionario(){
+  // seleção dos funcionários
+  opcao.forEach(opcao => {
+    opcao.addEventListener('click', () => {
+      // Adiciona ou remove a classe 'selected'
+      opcao.classList.toggle('selected');
+
+      // Adiciona ou remove a opção
+      if (opcao.classList.contains('selected')) {
+        selecionaOpcao.push(opcao);
+      
+      } else {
+      selecionaOpcao = selecionaOpcao.filter(opt => opt !== opcao);
+      selecionaOpcao.classList.remove('selected');
+      }
+    });
   });
-});
 
-//seleção da urgencia
-opcaoUrgencia.forEach(opcaoUrgencia =>{
-  opcaoUrgencia.addEventListener('click', () =>{  
-     if (selecionaUrgencia) {
-      selecionaUrgencia.classList.remove('selected');
-    }
+    //seleção da urgencia
+  opcaoUrgencia.forEach(opcaoUrgencia =>{
+    opcaoUrgencia.addEventListener('click', () =>{  
+      if (selecionaUrgencia) {
+        selecionaUrgencia.classList.remove('selected');
+      }
 
-    selecionaUrgencia = opcaoUrgencia;
-    opcaoUrgencia.classList.add('selected');
-  })
-})
+      selecionaUrgencia = opcaoUrgencia;
+      opcaoUrgencia.classList.add('selected');
+    })
+  });
+}
+
+// Executando todas as funções
+selecaoFuncionario();
+
+console.log(selecionaOpcao)
