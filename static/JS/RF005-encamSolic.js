3dbb29d802cba3206710232910cc4b572bae7637
Julia Ribeiro <julia.barros4336@gmail.com>
2024-09-26 10:29:56 -0300

    JS tela RF006

--- static/JS/RF005-encamSolic.js
+++ static/JS/RF005-encamSolic.js
@@ -1,30 +1,61 @@
+// const opcao = document.querySelectorAll('.opcao');
+// const urgencyOptions = document.querySelectorAll('.urgency');
+// let selectedOptions = [];
+// let selectedUrgency = null;
 
-const opcao = document.querySelectorAll('.opcao');
-const opcaoUrgencia  = document.querySelectorAll('.opcaoUrgencia');
+// opcao.forEach(opcao => {
+//   opcao.addEventListener('click', () => {
+//     // Adiciona ou remove a classe 'selected' dependendo do estado atual
+//     opcao.classList.toggle('selected');
 
-let selecionaOpcao = '';
-let selecionaUrgencia = '';
+//     // Adiciona ou remove a opção do array de opções selecionadas
+//     if (opcao.classList.contains('selected')) {
+//       selectedOptions.push(opcao);
+//     } else {
+//       selectedOptions = selectedOptions.filter(opt => opt !== opcao);
+//     }
+//   });
+// });
 
+// console.log(selectedOptions)
+
+// // function selecaoFuncionario(){
+// //   // seleção dos funcionários
+// //   opcao.forEach(opcao => {
+// //     opcao.addEventListener('change', () => {
+     
+// //       for(const option of opcao.options)
+
+// //     });
+// //   });
+
+// //     //seleção da urgencia
+// //   opcaoUrgencia.forEach(opcaoUrgencia =>{
+// //     opcaoUrgencia.addEventListener('click', () =>{  
+      
+      
+// //     })
+// //   });
+// // }
+
+// // // Executando todas as funções
+// // selecaoFuncionario();
 
 function selecaoFuncionario(){
-  // seleção dos funcionários
-  opcao.forEach(opcao => {
-    opcao.addEventListener('change', () => {
-     
-      for(const option of opcao.options)
-
-    });
-  });
-
-    //seleção da urgencia
-  opcaoUrgencia.forEach(opcaoUrgencia =>{
-    opcaoUrgencia.addEventListener('click', () =>{  
-      
-      
-    })
-  });
+  $.ajax({
+    url: '/RF005-retorna-funcionarios',
+    type: 'GET',
+    success: function(retorna_funcionarios){
+        for (let x = 0; x < retorna_funcionarios.length; x++) {
+          console.log(retorna_funcionarios[x])
+            // var option = document.createElement('option');
+            // option.value = servicos[x][0];
+            // option.textContent = servicos[x][1];
+            // inputServico.append(option);
+        }
+    },
+    error: function(){
+        alert(')X ERROOOOOOOOOO X(');
+    }
+});
 }
-
-// Executando todas as funções
-selecaoFuncionario();
-
