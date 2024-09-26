4b2068b6bd72ccb35c84cd93d314088d6d078409
Guilherme Mitsuyuki <guilherme.mitsuyuki@gmail.com>
2024-09-26 14:53:57 -0300

    fazendo saudação

---
+++ static/JS/funcao-horas.js
@@ -0,0 +1,14 @@
+function obterSaudacao() {
+    const agora = new Date();
+    const hora = agora.getHours();
+
+    if (hora >= 6 && hora < 12) {
+        return "Bom dia!";
+    } else if (hora >= 12 && hora < 18) {
+        return "Boa tarde!";
+    } else {
+        return "Boa noite!";
+    }
+}
+
+document.getElementById('saudacao').textContent = obterSaudacao();
