d7e2037d523f63fc327ef4565c612a9413b808ec
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-09-28 21:28:58 -0300

    MUdanças mínimas

--- Cadastro_ADM.py
+++ Cadastro_ADM.py
@@ -1,34 +1,35 @@
-# Importação das funções para conectar o MySQL e criar senhas criptografadas.
-from conexao_SQL import Connection
-from hashlib import sha256
+# # Importação das funções para conectar o MySQL e criar senhas criptografadas.
+# from conexao_SQL import Connection
+# from hashlib import sha256
 
-# Criando as importações do Flask
-from flask import Flask, render_template, request, redirect, session, jsonify
+# # Criando as importações do Flask
+# from flask import Flask, render_template, request, redirect, session, jsonify
 
-# Instanciando o WebService
-app = Flask(__name__)
+# # Instanciando o WebService
+# app = Flask(__name__)
 
-def cadastrarAdm():
-    print("Cadastrando ADM")
-    # myBD = Connection.conectar()
+# def cadastrarAdm():
+#     myBD = Connection.conectar()
 
-    # mycursor = myBD.cursor()
+#     mycursor = myBD.cursor()
 
-    # cpf = "43660542806"
-    # nome = "Gabriel ChagasFErnandes de Moraes"
-    # email = "gabriel.moraes4299@gmail.com"
-    # senha = "gabriel123"
-    # sn = "SN123456"
-    # foto = "foto"
-    # permissao = "administrador"
-    # id_funcao = ""
-    # logado = True
-    # senha_criptografada = sha256(senha.encode()).hexdigest()
+#     cpf = "43660542806"
+#     nome = "Gabriel ChagasFErnandes de Moraes"
+#     email = "gabriel.moraes4299@gmail.com"
+#     senha = "gabriel123"
+#     sn = "SN123456"
+#     foto = "foto"
+#     permissao = "administrador"
+#     id_funcao = ""
+#     logado = True
+#     senha_criptografada = sha256(senha.encode()).hexdigest()
 
-    # mycursor.execute(f"INSERT INTO tb_funcionarios (CPF_funcionario, nome, email, senha, SN, foto, permissao) VALUES ('{cpf}','{nome}', '{email}', '{senha_criptografada}', '{sn}', '{foto}', '{permissao}');")
+#     mycursor.execute(f"INSERT INTO tb_funcionarios (CPF_funcionario, nome, email, senha, SN, foto, permissao) VALUES ('{cpf}','{nome}', '{email}', '{senha_criptografada}', '{sn}', '{foto}', '{permissao}');")
 
-    # myBD.commit()
-cadastrarAdm()
+#     myBD.commit()
+# cadastrarAdm()
 
-#Rodando o WebService
-app.run(debug=True)
+# #Rodando o WebService
+# app.run(debug=True)
+
+print("Hello World!!!")
