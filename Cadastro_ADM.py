50f279b68de7897f61872bfab82e8a6f63aa1696
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-09-28 21:35:34 -0300

    Início e finalização da função para cadastrar o Administrador

--- Cadastro_ADM.py
+++ Cadastro_ADM.py
@@ -1,35 +1,30 @@
-# # Importação das funções para conectar o MySQL e criar senhas criptografadas.
-# from conexao_SQL import Connection
-# from hashlib import sha256
+# Importação das funções para conectar o MySQL e criar senhas criptografadas.
+from conexao_SQL import Connection
+from hashlib import sha256
 
-# # Criando as importações do Flask
-# from flask import Flask, render_template, request, redirect, session, jsonify
+def cadastrarAdm():
+    try:
+        myBD = Connection.conectar()
 
-# # Instanciando o WebService
-# app = Flask(__name__)
+        mycursor = myBD.cursor()
 
-# def cadastrarAdm():
-#     myBD = Connection.conectar()
+        cpf = "43660542806"
+        nome = "Gabriel Chagas Fernandes de Moraes"
+        email = "gabriel.moraes4299@gmail.com"
+        senha = "gabriel123"
+        sn = "SN123456"
+        foto = "foto"
+        permissao = "administrador"
+        senha_criptografada = sha256(senha.encode()).hexdigest()
 
-#     mycursor = myBD.cursor()
+        mycursor.execute(f"INSERT INTO tb_funcionarios (CPF_funcionario, nome, email, senha, SN, foto, permissao) VALUES ('{cpf}','{nome}', '{email}', '{senha_criptografada}', '{sn}', '{foto}', '{permissao}');")
 
-#     cpf = "43660542806"
-#     nome = "Gabriel ChagasFErnandes de Moraes"
-#     email = "gabriel.moraes4299@gmail.com"
-#     senha = "gabriel123"
-#     sn = "SN123456"
-#     foto = "foto"
-#     permissao = "administrador"
-#     id_funcao = ""
-#     logado = True
-#     senha_criptografada = sha256(senha.encode()).hexdigest()
+        myBD.commit()
 
-#     mycursor.execute(f"INSERT INTO tb_funcionarios (CPF_funcionario, nome, email, senha, SN, foto, permissao) VALUES ('{cpf}','{nome}', '{email}', '{senha_criptografada}', '{sn}', '{foto}', '{permissao}');")
-
-#     myBD.commit()
-# cadastrarAdm()
-
-# #Rodando o WebService
-# app.run(debug=True)
-
-print("Hello World!!!")
+        return True
+    except:
+        return False
+if cadastrarAdm():
+    print("Usuário cadastrado com Sucesso")
+else:
+    print("Erro ao cadastrar Administrador")
