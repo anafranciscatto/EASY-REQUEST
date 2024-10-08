d922180520166dc40a7b38b9119f0ef729558011
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-10-08 18:37:24 -0300

    Mudança no banco de dados e alteração na conexão

--- conexao_SQL.py
+++ conexao_SQL.py
@@ -2,7 +2,7 @@ import mysql.connector
 class Connection:
     def conectar():
         myBD = mysql.connector.connect(
-            host = "127.0.0.1",
+            host = "bd-easy-request.mysql.database.azure.com",
             user="equipe",
             password="4_b4t@t45_3_m31@",
             database="bd_easyrequest"
