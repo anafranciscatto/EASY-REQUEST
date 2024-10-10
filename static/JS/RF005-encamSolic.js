b405ba6ea1542388b0b24dbaa65224c3184463ef
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-10-10 14:16:31 -0300

    Finalização da correção dos erros do RF005

--- static/JS/RF005-encamSolic.js
+++ static/JS/RF005-encamSolic.js
@@ -15,7 +15,7 @@ function mostraFuncionarios(){
           div.className = 'caixa-opcao';
           div.id = `${retorna_funcionarios[x][2]}`
           div.setAttribute('onclick', `selecionarFuncionario('${retorna_funcionarios[x][2]}');`);
-          if (x[3] = 'à fazer') {
+          if (retorna_funcionarios[x][3] == 'à fazer') {
             div.innerHTML = `<figure><img src="https://pics.craiyon.com/2023-06-27/287f2a60c2e74386b5a89c517eb527dc.webp" alt="Imagem de Perfil"></figure>
                                         <div class="opcao_user"><p>${retorna_funcionarios[x][0]}</p>${retorna_funcionarios[x][1]}</div>
                                         <p class="status_disponivel">Disponível</p>`;
@@ -24,7 +24,7 @@ function mostraFuncionarios(){
                             <div class="opcao_user"><p>${retorna_funcionarios[x][0]}</p>${retorna_funcionarios[x][1]}</div>
                             <p class="status_n-disponivel">Não Disponível</p>`;
           }
-          
+
           divFuncionarios.append(div);
         }
     },
