a4bebc3036ca0e2a9a100cdb5d6ae4649b0ffb5a
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-10-13 19:09:13 -0300

    REcriando função redirectSolicitacao

--- static/JS/TLadministrador.js
+++ static/JS/TLadministrador.js
@@ -2,6 +2,10 @@ document.getElementById('menuHamburguer').addEventListener('click', function() {
     document.getElementById('menuList').classList.toggle('active');
 });
 
+function redirectSolicitacao(params) {
+    window.location.href = '/RF003';
+}
+
 function logout() {
     window.location.href = '/logout';
 }
