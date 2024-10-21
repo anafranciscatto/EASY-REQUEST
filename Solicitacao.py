416518976f68f6deb3da70eac57bd9d4880b6326
Gabriel Chagas Fernandes de Moraes <gabriel.moraes4299@gmail.com>
2024-10-21 20:05:18 -0300

    Ordenando solicitações

--- Solicitacao.py
+++ Solicitacao.py
@@ -40,7 +40,7 @@ class Solicitacao:
             myBD = Connection.conectar()
             mycursor = myBD.cursor()
 
-            sql =(f"SELECT s.id_solicitacao, sv.nome, id_sala, descricao, f.nome, IF(fn.id_funcao IS NULL, 'Administrador', fn.nome) AS nome_funcao , DATE_FORMAT(s.data_inicio, '%d/%m/%Y') AS data_inicio_brasileira FROM tb_solicitacoes s JOIN tb_funcionarios f ON f.CPF_funcionario = s.CPF_funcionario LEFT JOIN tb_funcoes fn ON fn.id_funcao = f.id_funcao JOIN tb_servicos sv ON sv.id_servico = s.id_servico LEFT JOIN tb_encaminhamentos enc ON enc.id_solicitacao = s.id_solicitacao WHERE enc.id_solicitacao IS NULL ORDER BY s.data_inicio;")
+            sql =(f"SELECT s.id_solicitacao, sv.nome, id_sala, descricao, f.nome, IF(fn.id_funcao IS NULL, 'Administrador', fn.nome) AS nome_funcao , DATE_FORMAT(s.data_inicio, '%d/%m/%Y') AS data_inicio_brasileira FROM tb_solicitacoes s JOIN tb_funcionarios f ON f.CPF_funcionario = s.CPF_funcionario LEFT JOIN tb_funcoes fn ON fn.id_funcao = f.id_funcao JOIN tb_servicos sv ON sv.id_servico = s.id_servico LEFT JOIN tb_encaminhamentos enc ON enc.id_solicitacao = s.id_solicitacao WHERE enc.id_solicitacao IS NULL ORDER BY s.data_inicio DESC;")
             mycursor.execute(sql)
             recebimento = mycursor.fetchall()
 
